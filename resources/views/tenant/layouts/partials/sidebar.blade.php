c2c1ad109fb3faf789649e1853f332e18497d158
Tu Nombre <derick061020@gmail.com>
2026-06-07 10:39:51 -0500

    1

--- resources/views/tenant/layouts/partials/sidebar.blade.php
+++ resources/views/tenant/layouts/partials/sidebar.blade.php
@@ -2239,10 +2239,11 @@ $showTransfer = collect($vc_module_levels)->intersect(['inventory', 'inventory_d
 
     .hotel-rooms-counter {
         display: inline-flex;
+        position: absolute;
         align-items: center;
         justify-content: center;
         margin-left: auto;
-        margin-right: 20px;
+        right: 40px;
         min-width: 20px;
         height: 20px;
         padding: 2px 6px;
