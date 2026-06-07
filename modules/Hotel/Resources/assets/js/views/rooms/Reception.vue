9bb7856cc101f522985ff70e4aeb239839f13198
Tu Nombre <derick061020@gmail.com>
2026-06-07 12:12:03 -0500

    cambios de color y boton para el checkin

--- modules/Hotel/Resources/assets/js/views/rooms/Reception.vue
+++ modules/Hotel/Resources/assets/js/views/rooms/Reception.vue
@@ -266,7 +266,7 @@
                                             class="btn btn-block btn-success px-0 py-2 w-100"
                                             style="color: black !important;"
                                             @click="onCheckIn(ro)"
-                                        >Check-in
+                                        >Reservado
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4" /><path d="M15 19l2 2l4 -4" /></svg>
                                         </button>
                                         <!-- Habitación ocupada: abre el modal de opciones -->
@@ -2668,8 +2668,8 @@ export default {
 
 /* Estilo para habitaciones con reserva (celeste tirando a azul) */
 .has-reservation {
-    background: rgb(53, 117, 229) !important;
-    border: 2px solid rgb(53, 117, 229) !important;
+    background: #0ea5e9  !important;
+    border: 2px solid #075985 !important;
     color: white !important;
     position: relative;
     overflow: hidden;
