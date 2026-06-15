53dce94dd1bf67f2f47751ff9987e90bc6fb93e3
Tu Nombre <derick061020@gmail.com>
2026-06-15 12:02:50 -0500

    solucion de api

--- modules/ApiPeruDev/Http/Controllers/ServiceController.php
+++ modules/ApiPeruDev/Http/Controllers/ServiceController.php
@@ -4,12 +4,15 @@ namespace Modules\ApiPeruDev\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use Modules\ApiPeruDev\Data\ServiceData;
+use Modules\Services\Data\DocumentApiResolver;
 
 class ServiceController extends Controller
 {
     public function service($type, $number)
     {
-        return (new ServiceData)->service($type, $number);
+        // Pasa por el resolver para respetar el proveedor configurado
+        // (apiperu / sunat) con fallback automatico a la otra API.
+        return (new DocumentApiResolver())->service($type, $number);
     }
 
     public function exchange($date)
