1c3b7599c1ccb2e6deb3319d4ef8896fd2e33878
Tu Nombre <derick061020@gmail.com>
2026-06-14 20:02:32 -0500

    mantenimiento

--- modules/Hotel/Resources/assets/js/views/rooms/Reception.vue
+++ modules/Hotel/Resources/assets/js/views/rooms/Reception.vue
@@ -185,7 +185,7 @@
                                     </div>
                                 </template>
                                 <template v-if="ro.status === 'MANTENIMIENTO'">
-                                    <h4 class="text-center mb-0" style="color:#000 !important;">
+                                    <h4 class="text-center mb-0" style="color: #ff1500 !important;">
                                     <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-tool"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 10h3v-3l-3.5 -3.5a6 6 0 0 1 8 8l6 6a2 2 0 0 1 -3 3l-6 -6a6 6 0 0 1 -8 -8l3.5 3.5" /></svg>
                                         <b>En mantenimiento:</b>
                                     </h4>
