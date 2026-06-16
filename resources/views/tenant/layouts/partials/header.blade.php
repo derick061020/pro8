b0812b4bdae62ba59acd4a4b34a719650b563e87
Tu Nombre <derick061020@gmail.com>
2026-06-15 22:41:08 -0500

    heade4r color

--- resources/views/tenant/layouts/partials/header.blade.php
+++ resources/views/tenant/layouts/partials/header.blade.php
@@ -497,9 +497,9 @@
                                 <a class="dropdown-item d-flex align-items-center" role="menuitem" href="{{route('tenant.payment.index')}}" style="color:#3b4a63 !important;">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round"
+                                        stroke-linejoin="round" style="color:#3b4a63 !important;"
                                         class="icon icon-tabler icons-tabler-outline icon-tabler-receipt-dollar me-2">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" style="color:#3b4a63 !important;" />
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"  />
                                         <path
                                             d="M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16l-3 -2l-2 2l-2 -2l-2 2l-2 -2l-3 2" />
                                         <path
