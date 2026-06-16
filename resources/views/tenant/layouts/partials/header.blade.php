de0b58541dbba9918df870c7c815557fbcca5a1d
Tu Nombre <derick061020@gmail.com>
2026-06-15 22:39:17 -0500

    header

--- resources/views/tenant/layouts/partials/header.blade.php
+++ resources/views/tenant/layouts/partials/header.blade.php
@@ -149,7 +149,7 @@
                                 <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round"
+                                        stroke-linejoin="round" style="color:#3b4a63 !important;"
                                         class="icon icon-tabler icons-tabler-outline icon-tabler-receipt-dollar me-2">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                         <path
