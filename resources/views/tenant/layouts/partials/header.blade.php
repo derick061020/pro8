33512c8711f672bcafdc2bb1a9768bda787c27ed
Tu Nombre <derick061020@gmail.com>
2026-06-15 22:38:23 -0500

    header

--- resources/views/tenant/layouts/partials/header.blade.php
+++ resources/views/tenant/layouts/partials/header.blade.php
@@ -499,14 +499,14 @@
                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                         stroke-linejoin="round"
                                         class="icon icon-tabler icons-tabler-outline icon-tabler-receipt-dollar me-2">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" style="color:#3b4a63 !important;" />
                                         <path
                                             d="M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16l-3 -2l-2 2l-2 -2l-2 2l-2 -2l-3 2" />
                                         <path
                                             d="M14.8 8a2 2 0 0 0 -1.8 -1h-2a2 2 0 1 0 0 4h2a2 2 0 1 1 0 4h-2a2 2 0 0 1 -1.8 -1" />
                                         <path d="M12 6v10" />
                                     </svg>
-                                    <span>Mis Pagos</span>
+                                    <span style="color:#3b4a63 !important;">Mis Pagos</span>
                                 </a>
                             </li>
                         @endif
@@ -515,7 +515,7 @@
                         <a class="dropdown-item d-flex align-items-center style-switcher-open" href="#" style="color:#3b4a63 !important;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round"
+                                stroke-linejoin="round" style="color:#3b4a63 !important;"
                                 class="icon icon-tabler icons-tabler-outline icon-tabler-paint me-2">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path
@@ -610,7 +610,7 @@
                             onclick="event.preventDefault(); document.getElementById('logout-form').submit();" style="color:#3b4a63 !important;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round"
+                                stroke-linejoin="round" style="color:#3b4a63 !important;"
                                 class="icon icon-tabler icons-tabler-outline icon-tabler-door-exit me-2">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M13 12v.01" />
